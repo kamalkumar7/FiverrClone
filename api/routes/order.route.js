71c8f5b70db6d5897207e22e951f72bebf46f079
hritik01478 <hritik01478@gmail.com>
2023-08-02 12:42:49 +0530

    message integrated

--- api/routes/order.route.js
+++ api/routes/order.route.js
@@ -1,10 +1,12 @@
-import express from "express"
-import {} from "../controllers/user.controller.js"
+import express from "express";
+import { verifyToken } from "../middleware/jwt.js";
+import { getOrders, intent, confirm } from "../controllers/order.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/test", (req, res) => {
-    res.send("it Works")
-})
+// router.post("/:gigId", verifyToken, createOrder);
+router.get("/", verifyToken, getOrders);
+router.post("/create-payment-intent/:id", verifyToken, intent);
+router.put("/", verifyToken, confirm);
 
 export default router;
